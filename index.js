997b6e1d4be514808584be5fe36d919c26d1219b
gasolin <gasolin@gmail.com>
2016-05-04 14:59:42 +0800

    Remove event listener in  method #2

--- index.js
+++ index.js
@@ -10,28 +10,42 @@ AFRAME.registerComponent('href', {
     default: ''
   },
 
+  boundClickHandler: undefined,
+
+  clickHandler: function href_clickHandler(url) {
+    var url = this.data;
+    console.log('link to ' + url);
+    if (url && url[0] === '#') { // in-page anchor
+      var ele = document.querySelector(url);
+      var cams = document.querySelectorAll('a-camera')
+      if (ele && cams) {
+        var targetPosition = ele.getAttribute('position');
+        console.log('focus camera to position:' +
+          JSON.stringify(targetPosition));
+        cams[0].setAttribute('position', targetPosition);
+        window.location.hash = url;
+      } else {
+        console.log('#id or a-camera is not defined');
+      }
+    } else { // normal hyper link
+      window.location.href = url;
+    }
+  },
+
   /**
    * Called once when component is attached. Generally for initial setup.
    */
   init: function() {
-    var url = this.data;
-    this.el.addEventListener('click', function() {
-      console.log('link to ' + url);
-      if (url && url[0] === '#') { // in-page anchor
-        var ele = document.querySelector(url);
-        var cams = document.querySelectorAll('a-camera')
-        if (ele && cams) {
-          var targetPosition = ele.getAttribute('position');
-          console.log('focus camera to position:' +
-            JSON.stringify(targetPosition));
-          cams[0].setAttribute('position', targetPosition);
-          window.location.hash = url;
-        } else {
-          console.log('#id or a-camera is not defined');
-        }
-      } else { // normal hyper link
-        window.location.href = url;
-      }
-    });
+    // var url = this.data;
+    this.boundClickHandler = this.clickHandler.bind(this);
+    this.el.addEventListener('click', this.boundClickHandler);
+  },
+
+  /**
+   * Called when a component is removed (e.g., via removeAttribute).
+   * Generally undoes all modifications to the entity.
+   */
+  remove: function () {
+    this.el.removeEventListener('click', this.boundClickHandler);
   }
 });
